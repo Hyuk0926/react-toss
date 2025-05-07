ba36c03b862d116ddfe61bede83aa07fe56f2fb2
hyuk <chldmsgur09@gmail.com>
2025-05-07 10:14:04 +0900

    Hero Animation 구현

--- app/routes/pages/home/components/hero.tsx
+++ app/routes/pages/home/components/hero.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'motion/react';
+
 import HeroDown from '~/components/svg/hero-down.svg?react';
 
 import AppButton from './app-button';
@@ -6,14 +8,24 @@ export default function Hero() {
   return (
     <section className="relative flex h-[calc(100vh-60px)] w-full flex-col items-center justify-start bg-[url('/images/new_main.png')] bg-cover bg-center pt-[150px]">
       <div className="hero-shadow absolute top-0 h-[500px] w-full" />
-      <div className="z-10 mb-[30px] text-center">
+      <motion.div
+        className="z-10 mb-[30px] text-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 0.7, ease: 'easeOut' }}
+      >
         <h1 className="text-[66px] leading-[1.4] font-bold text-black">
           금융의 모든 것<br />
           토스에서 쉽고 간편하게
         </h1>
-      </div>
+      </motion.div>
 
-      <div className="z-10 mb-[50px] flex w-full max-w-[600px] items-center justify-center gap-1">
+      <motion.div
+        className="z-10 mb-[50px] flex w-full max-w-[600px] items-center justify-center gap-1"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 1, duration: 0.7, ease: 'easeOut' }}
+      >
         <AppButton>
           <img src="/images/apple.png" alt="apple" width={24} height={24} />
           App Store
@@ -22,10 +34,29 @@ export default function Hero() {
           <img src="/images/google.png" alt="google" width={24} height={24} />
           Google Play
         </AppButton>
-      </div>
-      <div className="absolute bottom-[30px] z-10 h-[50px] w-[50px]">
+      </motion.div>
+      <motion.div
+        className="absolute bottom-[45px] z-10 h-[50px] w-[50px]"
+        initial={{ opacity: 0, scale: 0.85 }}
+        animate={{ opacity: 1, y: [0, 15, 0], scale: 1 }}
+        transition={{
+          opacity: {
+            duration: 1,
+            repeat: Infinity,
+            repeatType: 'reverse',
+            ease: 'easeOut',
+          },
+          scale: {
+            duration: 1,
+            repeat: Infinity,
+            repeatType: 'reverse',
+            ease: 'easeInOut',
+          },
+          y: { duration: 2, repeat: Infinity, repeatType: 'loop', ease: 'easeInOut' },
+        }}
+      >
         <HeroDown />
-      </div>
+      </motion.div>
     </section>
   );
 }
