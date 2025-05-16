b145cf207a812e64f97b206a763eedbaf181e025
jin <crispy4343@gmail.com>
2025-05-16 13:20:13 +0900

    관리자 레이아웃 적용

--- app/routes.ts
+++ app/routes.ts
@@ -1,4 +1,10 @@
-import { index, layout, prefix, route, type RouteConfig } from '@react-router/dev/routes';
+import {
+  index,
+  layout,
+  prefix,
+  route,
+  type RouteConfig,
+} from '~?react-router/dev/routes';
 
 // TODO: 경로 정의 (https://reactrouter.com/start/framework/routing)
 export default [
@@ -7,6 +13,15 @@ export default [
     index('routes/pages/home/route.tsx'),
     route('notice', 'routes/pages/notice/route.tsx'),
     route('notice/:id', 'routes/pages/notice-details/route.tsx'),
+  ]),
+
+  // * Admin
+  ...prefix('admin', [
+    layout('routes/layouts/admin-layout.tsx', [
+      index('routes/pages/admin/route.tsx'),
+      route('notice', 'routes/pages/admin-notice/route.tsx'),
+    ]),
+    route('login', 'routes/pages/admin-login/route.tsx'),
   ]),
 
   // * APIs
